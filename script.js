31eef8311d34183e9259241681c9dd27d1b7330c
Parlgy <you@example.com>
2022-11-15 22:56:28 +0300

    added customized colors

---
+++ script.js
@@ -0,0 +1,14 @@
+
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#DEB853',
+          secondary: '#789F8A',
+          accent: '#107563',
+          less: '#E2D9A2',
+          myblack: '#111111',
+        }
+      }
+    }
+  }
